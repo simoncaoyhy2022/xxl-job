e085dab31608cbc8821cc442a12a6588acd1358a
Simon_Cao <simon_cao@pmpgc.com>
2026-08-11 13:37:40 +0800

    常规同步

--- xxl-job-executor-samples/xxl-job-executor-etl/src/main/java/com/xxl/job/executor/config/MailAccountUtil.java
+++ xxl-job-executor-samples/xxl-job-executor-etl/src/main/java/com/xxl/job/executor/config/MailAccountUtil.java
@@ -1,20 +1,43 @@
 package com.xxl.job.executor.config;
 
 import cn.hutool.extra.mail.MailAccount;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MailAccountUtil {
+
+    @Value("${etl.mail.host}")
+    private String host;
+
+    @Value("${etl.mail.port}")
+    private int port;
+
+    @Value("${etl.mail.from}")
+    private String from;
+
+    @Value("${etl.mail.user}")
+    private String user;
+
+    @Value("${etl.mail.pass}")
+    private String pass;
+
+    @Value("${etl.mail.ssl-enable:true}")
+    private boolean sslEnable;
+
+    @Value("${etl.mail.starttls-enable:false}")
+    private boolean starttlsEnable;
+
     // hutool mail account
     public MailAccount getMailAccount() {
         return new MailAccount()
-                .setHost("smtp.mxhichina.com")
-                .setPort(465)                                     // 使用 SSL 端口
-                .setAuth(true)                                    // 必须开启认证
-                .setFrom("******")                                // 发件人邮箱
-                .setUser("******")                                // 登录用户名（完整邮箱）
-                .setPass("******************")                    // 客户端授权码/密码
-                .setSslEnable(true)                               // 启用 SSL 加密
-                .setStarttlsEnable(false);                        // SSL 和 STARTTLS 二选一
+                .setHost(host)
+                .setPort(port)
+                .setAuth(true)                       // 必须开启认证
+                .setFrom(from)                       // 发件人邮箱
+                .setUser(user)                       // 登录用户名（完整邮箱）
+                .setPass(pass)                       // 客户端授权码/密码
+                .setSslEnable(sslEnable)             // 启用 SSL 加密
+                .setStarttlsEnable(starttlsEnable);  // SSL 和 STARTTLS 二选一
     }
 }
